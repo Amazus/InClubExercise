11bd2ad1db8ec7cf95f32a7b67ed7e1c3cc0bd62
PheonixScale9094 <9094csw@gmail.com>
2022-03-07 16:41:05 -0500

    Update CubeController.cs
    The cube can  not be angry

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
+    bool IsAngry { get { return _isAngry; } set { ToggleAnger(value); _isAngry = value; } }
+    bool _isAngry;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<MeshRenderer>().material.color = Color.white;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && !IsAngry)
+        {
+            IsAngry = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space) && IsAngry)
+        {
+            IsAngry = false;
+        }
+    }
+
+    void ToggleAnger(bool isAngry)
+    {
+        if (isAngry)
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material.color = Color.white;
+        }
     }
 }
